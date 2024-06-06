538832427f15f8bf8cfb179f662cf2781a88754c
ElReyUnicornio <119967605+ElReyUnicornio@users.noreply.github.com>
2024-06-05 19:45:18 -0600

    fix: dome shader

--- GameManager.gd
+++ GameManager.gd
@@ -1,11 +1,23 @@
-extends Node3D
+extends Node
 
+var paused = false
+
+signal game_start()
+signal game_end()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func start_game():
+	game_start.emit()
+	
+func pause_game():
+	paused = !paused
+	
+func end_game():
+	game_end.emit()
